9f930cca9ca2f98d552e614e6d53e36db12c4b7b
Alok kumar mishra <62813608+sagaralok@users.noreply.github.com>
2026-04-16 18:10:10 +0530

    Add: binary-tree-inorder-traversal - submission-0

---
+++ Data Structures & Algorithms/binary-tree-inorder-traversal/submission-0.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void fun(TreeNode* root, vector<int>&arr){
+        if(root==NULL){
+            return ;
+        }
+        fun(root->left,arr);
+        arr.push_back(root->val);
+        fun(root->right,arr);
+    }
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int>ans;
+        fun(root,ans);
+        return ans;
+    }
+};
