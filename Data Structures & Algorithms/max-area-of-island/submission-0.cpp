ec2be31379ffab1387dafe25ad89af52ad42c78f
Alok kumar mishra <62813608+sagaralok@users.noreply.github.com>
2026-04-21 00:27:10 +0530

    Add: max-area-of-island - submission-0

---
+++ Data Structures & Algorithms/max-area-of-island/submission-0.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+
+    int xDirection[4] = {-1,0, 1, 0};
+    int yDirection[4] = {0, 1, 0,-1};
+
+    bool isValid(int x, int y, int n, int m){
+        if((x>=0 && x<n) && (y>=0 && y<m)){
+            return true;
+        }
+        return false;
+    }
+
+    int dfs(vector<vector<int>>&vis, vector<vector<int>>&grid, int x, int y){
+        vis[x][y]=1;
+        int ans = 1;
+        for(int i = 0;i<4;i++){
+            int newX = x+xDirection[i];
+            int newY = y+yDirection[i];
+            if(isValid(newX,newY, grid.size(), grid[0].size()) && grid[newX][newY]==1 
+                && vis[newX][newY] ==0){
+                ans+=dfs(vis,grid,newX,newY);
+            }
+        }
+        return ans;
+    }
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+        int ans = 0;
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<vector<int>>vis(n+1,vector<int>(m+1,0));
+        for(int i = 0;i<n;i++){
+            for(int j = 0;j<m;j++){
+                if(grid[i][j] == 1 && vis[i][j]==0){
+                    ans = max(ans,dfs(vis, grid, i, j));
+                }
+            }
+        }
+        return ans;
+    }
+};
